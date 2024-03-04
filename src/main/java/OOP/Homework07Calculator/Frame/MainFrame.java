6decdcf69ee929f7c2f0ee85ed4112259ec98092
ArtemFilosof <afilosof1@gmail.com>
2024-03-04 23:39:22 +0300

    OOP homework07

---
+++ src/main/java/OOP/Homework07Calculator/Frame/MainFrame.java
@@ -0,0 +1,20 @@
+package OOP.Homework07Calculator.Frame;
+
+import javax.swing.*;
+
+public class MainFrame {
+    static JFrame frame;
+    static JTextField result;
+
+    public static void main(String[] args) {
+        frame = new JFrame("Calc");
+        frame.setSize(720,1080);
+
+        result = new JTextField(16);
+        result.setEditable(false);
+        result.setText("hello");
+        frame.add(result);
+        frame.setVisible(true);
+
+    }
+}
