e94f099a2a91b5723bab62a726cacfc3400fdbcb
ArtemFilosof <afilosof1@gmail.com>
2024-03-05 19:58:41 +0300

    OOP homework07 readme+

--- src/main/java/OOP/Homework07Calculator/Main.java
+++ src/main/java/OOP/Homework07Calculator/Main.java
@@ -5,18 +5,16 @@ import OOP.Homework07Calculator.Controller.UserController;
 public class Main {
     public static void main(String[] args) {
 
-
-
-
         UserController userController = new UserController();
-        userController.help();
+        userController.help();//Вывод справки
+
         while (userController.getRun()){
 
             String expretion = userController.getUserInput();//Ввод строки
-            userController.parseInput(expretion); // разделяем троку
+            userController.parseInput(expretion);//разделяем троку
             userController.showParsed();//Вывод результата разделения
-            userController.getResult();
-            userController.printResult();
+            userController.getResult();//Вычисление
+            userController.printResult(); //вывод результата
         }
     }
 }
